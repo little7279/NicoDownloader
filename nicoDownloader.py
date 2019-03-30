bfd1aeb6a6daa8f5d2e08a106d14c115fc16940c
{Yu(_)³i}² <39945608+tladudtn@users.noreply.github.com>
2019-03-30 14:33:59 +0900

    3/30

--- nicoDownloader.py
+++ nicoDownloader.py
@@ -1,39 +1,43 @@
-import nicotools, getpass, os, ffmpeg
+import nicotools, getpass, os, shutil, moviepy.editor as mp
 
-# File Directory
-DIR = "./Downloads"
-
-
-# NicoVideo의 비디오 고유번호
-# Ex
-# https://www.nicovideo.jp/watch/sm5865063
-# -> 고유번호 : sm5865063
-SM = ''
-
-
-# 니코동에 로그인
+# JUUUUUUUUUST Login
 EMAIL = input('EMAIL:')
 PW = getpass.getpass('PW:')
-
+DIR = './Downloads/'
+DIR_M = './Music'
+SM = ''
 
 def VideoDownload(EMAIL, PW, SM):
-
-    print('if [nico URL]/sm32492001\n TYPE : 32492001  ')
     SM = input('sm')
     os.system('nicotools  download --mail "{0}" --pass "{1}" -v "sm{2}" -d "{3}"'.format(EMAIL, PW, SM, DIR))
+    
+    
 
 # 0. ./Downloads 폴더 안에 있는 파일 나열
 # 1.  변환할 파일 선택
 # 2.  변환후 ./Music로 이동
-
 def VideoToMusic():
+    i = 0
+
     file_list = os.listdir(DIR)
     print('FileList')
     for list in file_list:
-        print('{0}'.format(list))
+        print('{0}. {1}'.format(i, list))
 
+    n =  int(input('NUMBER :'))
+
+    clip = mp.VideoFileClip(DIR+file_list[n]).subclip(0, -1)
+    music = file_list[n].replace('.mp4', '.mp3')
+    clip.audio.write_audiofile(music)
+    print('Video -> Music Complete')
+    
 while True:
-    print('[MENU]\n1. Video Download\n2. movie -> music\n3. EDIT your MYLIST\n4. press q -> exit')
+
+    print('[MENU]\n'
+          '1. Video Download\n'
+          '2. Video -> Music\n'
+          '3. MyList\n'
+          '4. Exit\n')
     menu = input('SELECT[1-3]: ')
     os.system('cls')
 
@@ -43,13 +47,6 @@ while True:
     elif menu == '2':
         VideoToMusic()
 
-    elif menu == 'q':
+    elif menu == '4':
         print('exit nicoDownloader')
         exit(0)
-
-'''
-
-/search/검색어
-
-/watch/sm34388038
-'''
